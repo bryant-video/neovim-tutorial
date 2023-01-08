1879e32400696f5d5aa8141706b31582249fe202
Bryant Zhou <btyzt0@vimtutorial.local>
2023-01-08 00:41:16 -0600

    change name

--- nvim/lua/bryant/core/options.lua
+++ nvim/lua/bryant/core/options.lua
@@ -10,7 +10,7 @@ opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
 -- line wrapping
-opt.wrap = false -- disable line wrapping
+opt.wrap = true
 
 -- search settings
 opt.ignorecase = true -- ignore case when searching
